1fc654c92e503f4affd1834b709100d8c160110d
Dmitry Afanasiev <dmitry.afanasiev@cigen.me>
2024-12-02 01:26:43 +0200

    Day #1

---
+++ day1/puzzle1/Program.cs
@@ -0,0 +1,12 @@
+﻿var lines = File.ReadAllLines("input.txt").Select(line => line.Split("   "));
+
+var leftList = lines.Select(line => int.Parse(line[0])).Order().ToList();
+var rightList = lines.Select(line => int.Parse(line[1])).Order().ToList();
+
+var distance = 0;
+for (int i = 0; i < leftList.Count; i++)
+{
+    distance += leftList[i] < rightList[i] ? rightList[i] - leftList[i] : leftList[i] - rightList[i];
+}
+
+Console.WriteLine(distance);
---
+++ day1/puzzle2/Program.cs
@@ -0,0 +1,21 @@
+﻿var lines = File.ReadAllLines("input.txt").Select(line => line.Split("   "));
+
+var leftList = lines.Select(line => int.Parse(line[0])).Order().ToList();
+var rightList = lines.Select(line => int.Parse(line[1])).Order().ToList();
+
+var similarity = 0;
+var freqMap = new Dictionary<int, int>();
+
+foreach (var lv in leftList)
+{
+    if (freqMap.TryGetValue(lv, out int freq)) {
+        similarity += lv * freq;
+    } else {
+        var lvFreq = rightList.Count(rv => rv == lv);
+        similarity += lv * lvFreq;
+
+        freqMap[lv] = lvFreq;
+    }
+}
+
+Console.WriteLine(similarity);
