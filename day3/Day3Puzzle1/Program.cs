018a1266a6e379f91106b9c2df8ce05f3a801bf5
Dmytro Afanasiev <attflederx@gmail.com>
2024-12-04 02:17:03 +0200

    Day #3

---
+++ day3/Day3Puzzle1/Program.cs
@@ -0,0 +1,10 @@
+﻿using System.Text.RegularExpressions;
+
+var data = File.ReadAllText("input.txt");
+var validOps = Regex.Matches(data, @"mul\((\d+),(\d+)\)");
+var result = validOps
+    .Select(op => new List<int> { int.Parse(op.Groups[1].Value), int.Parse(op.Groups[2].Value) })
+    .Select(arr => arr[0] * arr[1])
+    .Sum();
+
+Console.WriteLine(result);
---
+++ day3/Day3Puzzle2/Program.cs
@@ -0,0 +1,13 @@
+﻿using System.Text.RegularExpressions;
+
+var data = File.ReadAllText("input.txt").Replace("\n", string.Empty);
+//var data = @"xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))don't()mul(28,11)aaa";
+var enabledInstructions = Regex.Replace(data, @"don't\(\).*?(do\(\)|$)", string.Empty);
+
+var validOps = Regex.Matches(enabledInstructions, @"mul\((\d+),(\d+)\)");
+var result = validOps
+    .Select(op => new List<int> { int.Parse(op.Groups[1].Value), int.Parse(op.Groups[2].Value) })
+    .Select(arr => arr[0] * arr[1])
+    .Sum();
+
+Console.WriteLine(result);
